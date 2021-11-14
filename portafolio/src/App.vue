1f62694d186594a2660e8148926408797ef7871c
John-Alexis <johnscuellar19@gmail.com>
2021-11-13 22:40:06 -0500

    act

---
+++ portafolio/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
